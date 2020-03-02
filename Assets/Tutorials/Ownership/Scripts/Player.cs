3e570c617e62985e7234f2538b70c060c295caba
Nathan <natemi1010@gmail.com>
2020-03-02 15:24:05 +0000

    Finished Ownership Tutorial

---
+++ Assets/Tutorials/Ownership/Scripts/Player.cs
@@ -0,0 +1,20 @@
+﻿using Mirror;
+using UnityEngine;
+
+namespace DapperDino.Mirror.Tutorials.Ownership
+{
+    public class Player : NetworkBehaviour
+    {
+        [SerializeField] private Vector3 movement = new Vector3();
+
+        [Client]
+        private void Update()
+        {
+            if (!hasAuthority) { return; }
+
+            if (!Input.GetKeyDown(KeyCode.Space)) { return; }
+
+            transform.Translate(movement);
+        }
+    }
+}
