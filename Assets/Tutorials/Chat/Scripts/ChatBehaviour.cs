cac19d724a3d855b50e1b17a02b581f4cfe38283
Nathan <natemi1010@gmail.com>
2020-03-04 14:18:17 +0000

    Finished recording Chat tutorial

---
+++ Assets/Tutorials/Chat/Scripts/ChatBehaviour.cs
@@ -0,0 +1,60 @@
+﻿using Mirror;
+using System;
+using TMPro;
+using UnityEngine;
+
+namespace DapperDino.Mirror.Tutorials.Chat
+{
+    public class ChatBehaviour : NetworkBehaviour
+    {
+        [SerializeField] private GameObject chatUI = null;
+        [SerializeField] private TMP_Text chatText = null;
+        [SerializeField] private TMP_InputField inputField = null;
+
+        private static event Action<string> OnMessage;
+
+        public override void OnStartAuthority()
+        {
+            chatUI.SetActive(true);
+
+            OnMessage += HandleNewMessage;
+        }
+
+        [ClientCallback]
+        private void OnDestroy()
+        {
+            if (!hasAuthority) { return; }
+
+            OnMessage -= HandleNewMessage;
+        }
+
+        private void HandleNewMessage(string message)
+        {
+            chatText.text += message;
+        }
+
+        [Client]
+        public void Send(string message)
+        {
+            if (!Input.GetKeyDown(KeyCode.Return)) { return; }
+
+            if (string.IsNullOrWhiteSpace(message)) { return; }
+
+            CmdSendMessage(message);
+
+            inputField.text = string.Empty;
+        }
+
+        [Command]
+        private void CmdSendMessage(string message)
+        {
+            RpcHandleMessage($"[{connectionToClient.connectionId}]: {message}");
+        }
+
+        [ClientRpc]
+        private void RpcHandleMessage(string message)
+        {
+            OnMessage?.Invoke($"\n{message}");
+        }
+    }
+}
