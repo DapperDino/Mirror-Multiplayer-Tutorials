93f93619484ade43af3d38efb5b3edfe27a73549
Nathan <natemi1010@gmail.com>
2020-03-09 14:41:25 +0000

    Finished recording Lobby System tutorial

--- Assets/Tutorials/Lobby/Scripts/NetworkManagerLobby.cs
+++ Assets/Tutorials/Lobby/Scripts/NetworkManagerLobby.cs
@@ -1,5 +1,6 @@
 ﻿using Mirror;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -8,6 +9,7 @@ namespace DapperDino.Mirror.Tutorials.Lobby
 {
     public class NetworkManagerLobby : NetworkManager
     {
+        [SerializeField] private int minPlayers = 2;
         [Scene] [SerializeField] private string menuScene = string.Empty;
 
         [Header("Room")]
@@ -15,6 +17,8 @@ namespace DapperDino.Mirror.Tutorials.Lobby
 
         public static event Action OnClientConnected;
         public static event Action OnClientDisconnected;
+
+        public List<NetworkRoomPlayerLobby> RoomPlayers { get; } = new List<NetworkRoomPlayerLobby>();
 
         public override void OnStartServer() => spawnPrefabs = Resources.LoadAll<GameObject>("SpawnablePrefabs").ToList();
 
@@ -61,10 +65,53 @@ namespace DapperDino.Mirror.Tutorials.Lobby
         {
             if (SceneManager.GetActiveScene().name == menuScene)
             {
+                bool isLeader = RoomPlayers.Count == 0;
+
                 NetworkRoomPlayerLobby roomPlayerInstance = Instantiate(roomPlayerPrefab);
+
+                roomPlayerInstance.IsLeader = isLeader;
 
                 NetworkServer.AddPlayerForConnection(conn, roomPlayerInstance.gameObject);
             }
+        }
+
+        public override void OnServerDisconnect(NetworkConnection conn)
+        {
+            if (conn.identity != null)
+            {
+                var player = conn.identity.GetComponent<NetworkRoomPlayerLobby>();
+
+                RoomPlayers.Remove(player);
+
+                NotifyPlayersOfReadyState();
+            }
+
+            base.OnServerDisconnect(conn);
+        }
+
+        public override void OnStopServer()
+        {
+            RoomPlayers.Clear();
+        }
+
+        public void NotifyPlayersOfReadyState()
+        {
+            foreach (var player in RoomPlayers)
+            {
+                player.HandleReadyToStart(IsReadyToStart());
+            }
+        }
+
+        private bool IsReadyToStart()
+        {
+            if (numPlayers < minPlayers) { return false; }
+
+            foreach (var player in RoomPlayers)
+            {
+                if (!player.IsReady) { return false; }
+            }
+
+            return true;
         }
     }
 }
--- Assets/Tutorials/Lobby/Scripts/NetworkRoomPlayerLobby.cs
+++ Assets/Tutorials/Lobby/Scripts/NetworkRoomPlayerLobby.cs
@@ -1,9 +1,125 @@
 ﻿using Mirror;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
 
 namespace DapperDino.Mirror.Tutorials.Lobby
 {
     public class NetworkRoomPlayerLobby : NetworkBehaviour
     {
-        
+        [Header("UI")]
+        [SerializeField] private GameObject lobbyUI = null;
+        [SerializeField] private TMP_Text[] playerNameTexts = new TMP_Text[4];
+        [SerializeField] private TMP_Text[] playerReadyTexts = new TMP_Text[4];
+        [SerializeField] private Button startGameButton = null;
+
+        [SyncVar(hook = nameof(HandleDisplayNameChanged))]
+        public string DisplayName = "Loading...";
+        [SyncVar(hook = nameof(HandleReadyStatusChanged))]
+        public bool IsReady = false;
+
+        private bool isLeader;
+        public bool IsLeader
+        {
+            set
+            {
+                isLeader = value;
+                startGameButton.gameObject.SetActive(value);
+            }
+        }
+
+        private NetworkManagerLobby room;
+        private NetworkManagerLobby Room
+        {
+            get
+            {
+                if (room != null) { return room; }
+                return room = NetworkManager.singleton as NetworkManagerLobby;
+            }
+        }
+
+        public override void OnStartAuthority()
+        {
+            CmdSetDisplayName(PlayerNameInput.DisplayName);
+
+            lobbyUI.SetActive(true);
+        }
+
+        public override void OnStartClient()
+        {
+            Room.RoomPlayers.Add(this);
+
+            UpdateDisplay();
+        }
+
+        public override void OnNetworkDestroy()
+        {
+            Room.RoomPlayers.Remove(this);
+
+            UpdateDisplay();
+        }
+
+        public void HandleReadyStatusChanged(bool oldValue, bool newValue) => UpdateDisplay();
+        public void HandleDisplayNameChanged(string oldValue, string newValue) => UpdateDisplay();
+
+        private void UpdateDisplay()
+        {
+            if (!hasAuthority)
+            {
+                foreach (var player in Room.RoomPlayers)
+                {
+                    if (player.hasAuthority)
+                    {
+                        player.UpdateDisplay();
+                        break;
+                    }
+                }
+
+                return;
+            }
+
+            for (int i = 0; i < playerNameTexts.Length; i++)
+            {
+                playerNameTexts[i].text = "Waiting For Player...";
+                playerReadyTexts[i].text = string.Empty;
+            }
+
+            for (int i = 0; i < Room.RoomPlayers.Count; i++)
+            {
+                playerNameTexts[i].text = Room.RoomPlayers[i].DisplayName;
+                playerReadyTexts[i].text = Room.RoomPlayers[i].IsReady ?
+                    "<color=green>Ready</color>" :
+                    "<color=red>Not Ready</color>";
+            }
+        }
+
+        public void HandleReadyToStart(bool readyToStart)
+        {
+            if (!isLeader) { return; }
+
+            startGameButton.interactable = readyToStart;
+        }
+
+        [Command]
+        private void CmdSetDisplayName(string displayName)
+        {
+            DisplayName = displayName;
+        }
+
+        [Command]
+        public void CmdReadyUp()
+        {
+            IsReady = !IsReady;
+
+            Room.NotifyPlayersOfReadyState();
+        }
+
+        [Command]
+        public void CmdStartGame()
+        {
+            if (Room.RoomPlayers[0].connectionToClient != connectionToClient) { return; }
+
+            // Start Game
+        }
     }
 }
