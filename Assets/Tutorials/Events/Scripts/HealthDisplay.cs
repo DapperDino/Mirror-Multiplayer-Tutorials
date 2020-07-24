eed1cfa2c814eff9ce7799d292fa6cbdf3e85aa9
Nathan <natemi1010@gmail.com>
2020-07-24 15:18:04 +0100

    End of events tutorial

---
+++ Assets/Tutorials/Events/Scripts/HealthDisplay.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace DapperDino.Mirror.Tutorials.Events
+{
+    public class HealthDisplay : MonoBehaviour
+    {
+        [Header("References")]
+        [SerializeField] private Health health = null;
+        [SerializeField] private Image healthBarImage = null;
+
+        private void OnEnable()
+        {
+            health.EventHealthChanged += HandleHealthChanged;
+        }
+
+        private void OnDisable()
+        {
+            health.EventHealthChanged -= HandleHealthChanged;
+        }
+
+        private void HandleHealthChanged(int currentHealth, int maxHealth)
+        {
+            healthBarImage.fillAmount = (float)currentHealth / maxHealth;
+        }
+    }
+}
