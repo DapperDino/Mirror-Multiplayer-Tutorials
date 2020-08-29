92a57a69f23cb6369815bc650a7b7f45463a358f
Nathan <natemi1010@gmail.com>
2020-08-29 16:04:33 +0100

    Finished Steamworks tutorial

---
+++ Assets/Tutorials/Steamworks/Scripts/MyNetworkManager.cs
@@ -0,0 +1,21 @@
+﻿using Mirror;
+using UnityEngine;
+
+namespace DapperDino.Mirror.Tutorials.Steamworks
+{
+    public class MyNetworkManager : NetworkManager
+    {
+        [SerializeField] private string notificationMessage = string.Empty;
+
+        public override void OnStartServer()
+        {
+            ServerChangeScene("Scene_SteamworksLobby");
+        }
+
+        [ContextMenu("Send Notification")]
+        private void SendNotification()
+        {
+            NetworkServer.SendToAll(new Notification { content = notificationMessage });
+        }
+    }
+}
---
+++ Assets/Tutorials/Steamworks/Scripts/SteamLobby.cs
@@ -0,0 +1,72 @@
+﻿using Mirror;
+using Steamworks;
+using UnityEngine;
+
+namespace DapperDino.Mirror.Tutorials.Steamworks
+{
+    public class SteamLobby : MonoBehaviour
+    {
+        [SerializeField] private GameObject buttons = null;
+
+        protected Callback<LobbyCreated_t> lobbyCreated;
+        protected Callback<GameLobbyJoinRequested_t> gameLobbyJoinRequested;
+        protected Callback<LobbyEnter_t> lobbyEntered;
+
+        private const string HostAddressKey = "HostAddress";
+
+        private NetworkManager networkManager;
+
+        private void Start()
+        {
+            networkManager = GetComponent<NetworkManager>();
+
+            if (!SteamManager.Initialized) { return; }
+
+            lobbyCreated = Callback<LobbyCreated_t>.Create(OnLobbyCreated);
+            gameLobbyJoinRequested = Callback<GameLobbyJoinRequested_t>.Create(OnGameLobbyJoinRequested);
+            lobbyEntered = Callback<LobbyEnter_t>.Create(OnLobbyEntered);
+        }
+
+        public void HostLobby()
+        {
+            buttons.SetActive(false);
+
+            SteamMatchmaking.CreateLobby(ELobbyType.k_ELobbyTypeFriendsOnly, networkManager.maxConnections);
+        }
+
+        private void OnLobbyCreated(LobbyCreated_t callback)
+        {
+            if (callback.m_eResult != EResult.k_EResultOK)
+            {
+                buttons.SetActive(true);
+                return;
+            }
+
+            networkManager.StartHost();
+
+            SteamMatchmaking.SetLobbyData(
+                new CSteamID(callback.m_ulSteamIDLobby),
+                HostAddressKey,
+                SteamUser.GetSteamID().ToString());
+        }
+
+        private void OnGameLobbyJoinRequested(GameLobbyJoinRequested_t callback)
+        {
+            SteamMatchmaking.JoinLobby(callback.m_steamIDLobby);
+        }
+
+        private void OnLobbyEntered(LobbyEnter_t callback)
+        {
+            if (NetworkServer.active) { return; }
+
+            string hostAddress = SteamMatchmaking.GetLobbyData(
+                new CSteamID(callback.m_ulSteamIDLobby),
+                HostAddressKey);
+
+            networkManager.networkAddress = hostAddress;
+            networkManager.StartClient();
+
+            buttons.SetActive(false);
+        }
+    }
+}
