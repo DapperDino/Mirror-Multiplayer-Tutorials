23d9e82733e478d8e531a3fad0f9f89b96a9abb9
Nathan <natemi1010@gmail.com>
2020-09-07 15:01:06 +0100

    Finished Steamworks Profile Data tutorial

---
+++ Assets/Tutorials/SteamProfile/Scripts/MyNetworkManager.cs
@@ -0,0 +1,19 @@
+﻿using Mirror;
+using Steamworks;
+
+namespace DapperDino.Mirror.Tutorials.SteamProfile
+{
+    public class MyNetworkManager : NetworkManager
+    {
+        public override void OnServerAddPlayer(NetworkConnection conn)
+        {
+            base.OnServerAddPlayer(conn);
+
+            CSteamID steamId = SteamMatchmaking.GetLobbyMemberByIndex(SteamLobby.LobbyId, numPlayers - 1);
+
+            var playerInfoDisplay = conn.identity.GetComponent<PlayerInfoDisplay>();
+
+            playerInfoDisplay.SetSteamId(steamId.m_SteamID);
+        }
+    }
+}
---
+++ Assets/Tutorials/SteamProfile/Scripts/PlayerInfoDisplay.cs
@@ -0,0 +1,80 @@
+﻿using Mirror;
+using Steamworks;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace DapperDino.Mirror.Tutorials.SteamProfile
+{
+    public class PlayerInfoDisplay : NetworkBehaviour
+    {
+        [SyncVar(hook = nameof(HandleSteamIdUpdated))]
+        private ulong steamId;
+
+        [SerializeField] private RawImage profileImage = null;
+        [SerializeField] private TMP_Text displayNameText = null;
+
+        protected Callback<AvatarImageLoaded_t> avatarImageLoaded;
+
+        #region Server
+
+        public void SetSteamId(ulong steamId)
+        {
+            this.steamId = steamId;
+        }
+
+        #endregion
+
+        #region Client
+
+        public override void OnStartClient()
+        {
+            avatarImageLoaded = Callback<AvatarImageLoaded_t>.Create(OnAvatarImageLoaded);
+        }
+
+        private void HandleSteamIdUpdated(ulong oldSteamId, ulong newSteamId)
+        {
+            var cSteamId = new CSteamID(newSteamId);
+
+            int ret = SteamFriends.GetLargeFriendAvatar(cSteamId);
+
+            displayNameText.text = SteamFriends.GetFriendPersonaName(cSteamId);
+
+            if (ret == -1) { return; }
+
+            profileImage.texture = GetSteamImageAsTexture(ret);
+        }
+
+        private void OnAvatarImageLoaded(AvatarImageLoaded_t callback)
+        {
+            if (callback.m_steamID.m_SteamID != steamId) { return; }
+
+            profileImage.texture = GetSteamImageAsTexture(callback.m_iImage);
+        }
+
+        private Texture2D GetSteamImageAsTexture(int iImage)
+        {
+            Texture2D texture = null;
+
+            bool isValid = SteamUtils.GetImageSize(iImage, out uint width, out uint height);
+
+            if (isValid)
+            {
+                byte[] image = new byte[width * height * 4];
+
+                isValid = SteamUtils.GetImageRGBA(iImage, image, (int)(width * height * 4));
+
+                if (isValid)
+                {
+                    texture = new Texture2D((int)width, (int)height, TextureFormat.RGBA32, false, true);
+                    texture.LoadRawTextureData(image);
+                    texture.Apply();
+                }
+            }
+
+            return texture;
+        }
+
+        #endregion
+    }
+}
